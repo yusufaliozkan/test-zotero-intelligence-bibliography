2e1b4e6f55be5d0df5a84ff763f24caa701db476
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-03-24 10:19:31 +0000

    g

--- Home.py
+++ Home.py
@@ -169,7 +169,6 @@ with tab1:
         st.header('Recently added or updated items: ')
             
         df_download = df.iloc[:, [0,1,2,4,5,6,9]] 
-        df_download
         df_download = df_download[['Title', 'Publication type', 'Authors', 'Link to publication', 'Zotero link', 'Date published', 'Date added']]
 
         def convert_df(df):
