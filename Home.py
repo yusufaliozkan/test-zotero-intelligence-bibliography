8b8a2dde0bd494254c6ff717aef81e5739c4a1ae
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-05-06 10:29:40 +0100

    jh

--- Home.py
+++ Home.py
@@ -51,26 +51,27 @@ def zotero_data(library_id, library_type):
     
     book_titles = []
     for item in items:
-        data.append((
-        item['data']['title'],
-        item['data']['itemType'], 
         creators = item['data']['creators']
         creators_str = ", ".join([creator.get('firstName', '') + ' ' + creator.get('lastName', '') for creator in creators])
         if item['data']['itemType'] == 'bookSection':
             book_titles.append(item.get('bookTitle'))
-        book_titles[-1] if book_titles else None, 
-        item['data']['url'], 
-        item['data']['abstractNote'], 
-        item['links']['alternate']['href'],
-        item['data']['dateAdded'],
-        item['data'].get('date'), 
-        item['data']['dateModified'],
-        item['data']['collections'],
-        creators_str,
-        item['data'].get('publicationTitle')
+        data.append((
+            item['data']['title'],
+            item['data']['itemType'], 
+            book_titles[-1] if book_titles else None, 
+            item['data']['url'], 
+            item['data']['abstractNote'], 
+            item['links']['alternate']['href'],
+            item['data']['dateAdded'],
+            item['data'].get('date'), 
+            item['data']['dateModified'],
+            item['data']['collections'],
+            creators_str,
+            item['data'].get('publicationTitle')
         ))
     df = pd.DataFrame(data, columns=columns)
     return df
+
 
 df = zotero_data(library_id, library_type)
 df
