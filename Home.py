7b7247d3426d956c951b3f4d19c378afc7496c40
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-05-06 10:11:11 +0100

    sd

--- Home.py
+++ Home.py
@@ -51,8 +51,10 @@ def zotero_data(library_id, library_type):
     for item in items:
         creators = item['data']['creators']
         creators_str = ", ".join([creator.get('firstName', '') + ' ' + creator.get('lastName', '') for creator in creators])
-        data.append((item['data']['title'], 
+        data.append((item['data']['title'],
         item['data']['itemType'], 
+        if item['itemType'] == 'bookSection':
+            book_titles.append(item.get('bookTitle'))
         item['data']['url'], 
         item['data']['abstractNote'], 
         item['links']['alternate']['href'],
