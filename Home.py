8c9a7f62e3ae756a720f81cd0342949172370ad5
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-11-02 23:25:09 +0000

    sd

--- Home.py
+++ Home.py
@@ -39,8 +39,6 @@ pd.set_option('display.max_colwidth', None)
 
 zot = zotero.Zotero(library_id, library_type)
 items = zot.top(limit=15)
-items 
-
 
 @st.cache_data(ttl=600)
 def zotero_data(library_id, library_type):
